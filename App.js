bfc6991dc824dfcdac217eb16e662f21fbd21279
PandikumarDev <developerpk7@gmail.com>
2023-10-24 21:54:07 +0530

    Parcel Installed

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const heading = React.createElement("h1", {id:"heading"}, "Hello world from React");
+import React from "react";
+import ReactDOM from "react-dom";
+const heading = React.createElement("h1", {id:"heading"}, "Hello  React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 console.log(heading)
